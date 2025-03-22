b8fbc3f34756e4b38a9a88a62bc7f9765dfe9cd0
Darius Spr <108625236+Dariusspr@users.noreply.github.com>
2025-03-22 19:04:43 +0200

    refactoring

---
+++ AimReactionAPI/DTOs/UserRegisterDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AimReactionAPI.DTOs;
+
+public class UserRegisterDto
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
